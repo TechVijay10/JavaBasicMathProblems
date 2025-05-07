45ca640c8ae4a832e6816e5b1a5075f566f46f78
TechVijay10 <vijay.2022.07@gmail.com>
2025-05-07 21:27:06 +0530

    Added FindingLastDigit Program

---
+++ src/com/basicmath/FindingLastDigit.java
@@ -0,0 +1,13 @@
+package com.basicmath;
+
+public class FindingLastDigit {
+
+	public static void main(String[] args) {
+
+		int number = 76342;
+		int lastDigit = number % 10;
+
+		System.out.println(lastDigit + " is the last digit");
+
+	}
+}
